b2ff0d81b9221d76110d74aed11b82d0206c106e
Qifan Wang <wqf995@gmail.com>
2019-01-22 22:52:25 -0500

    Updated analysis 1/22

--- analysis.R
+++ analysis.R
@@ -46,3 +46,65 @@ SalesByTime = orders %>%
 
 g = ggplot(SalesByTime, aes(week, totalsales))
 g + geom_line()
+
+#Category analysis
+productnames = products %>%
+        select(product_id, product_category_name) %>%
+        left_join(categorynames, by = 'product_category_name') %>%
+        select(-2)
+
+SalesByCategory = items %>%
+        left_join(productnames, by = 'product_id') %>%
+        group_by(product_category_name_english) %>%
+        summarise(revenue = sum(price)) %>%
+        arrange(desc(revenue))
+
+c = ggplot(SalesByCategory, aes(x = "", revenue, fill = product_category_name_english))
+c + geom_bar(width = 1, stat = 'identity')
+c + coord_polar("y", start = 0)
+
+#Cleaning Geographical data
+c2 = customer %>%
+        group_by(customer_zip_code_prefix) %>%
+        summarise(n = n())
+
+
+
+g2 = geolocation %>%
+        distinct(geolocation_zip_code_prefix)
+summary(g2)
+summary(g1$geolocation_zip_code_prefix)
+g1 = geolocation %>%
+        select(geolocation_zip_code_prefix, geolocation_lat, geolocation_lng) %>%
+        group_by(geolocation_zip_code_prefix) %>%
+        summarise(lat = mean(geolocation_lat), lng = mean(geolocation_lng))
+
+#Cleaning Payment Table
+p1 = payments %>%
+        group_by(order_id) %>%
+        summarise(value = sum(payment_value))
+
+#Joining all tables
+o1 = orders %>%
+        left_join(p1, by = "order_id") %>%
+        left_join(customer, by = "customer_id") %>%
+        left_join(g1, by = c("customer_zip_code_prefix" = "geolocation_zip_code_prefix")) %>%
+        left_join(reviews, by = "order_id") %>%
+        left_join(items, by = "order_id") %>%
+        left_join(productnames, by = "product_id") %>%
+        select(order_id, customer_id, order_status,order_purchase_timestamp, order_delivered_customer_date, 
+               value, customer_unique_id, customer_zip_code_prefix, lat, lng, customer_city, customer_state, 
+               review_score, product_id, product_category_name_english)
+
+#Cleaning Gaint Table Data
+o2 = o1 %>%
+        filter(order_status != "canceled") %>%
+        rename(purchase_date = order_purchase_timestamp, delivered_date = order_delivered_customer_date, 
+               zip = customer_zip_code_prefix, city = customer_city, state = customer_state, 
+               product_category = product_category_name_english) %>%
+        mutate(purchase_date = as_datetime(purchase_date), delivered_date = as_datetime(delivered_date))
+
+class(o2$purchase_date)
+?left_join
+o1 = orders %>%
+        group_by()
